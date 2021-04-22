56295eae9623cf27ea724f555104fcfce7cf9046
Farrukh Shahid <hafiz-farrukh.shahid@autocanvas.ai>
2021-04-22 17:43:55 +0500

    object coliding and draging

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacles")
+        {
+            Debug.Log("We hit player 1");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public GameObject player;
+
+    public float forwardForce = 2000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Debug.Log("Hello World!!!");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            // move right
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            // move right
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
